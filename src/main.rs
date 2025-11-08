f28715f7c92c3ef9d3e71fc32cce01fbc43c61aa
TPride <tranielmp8@gmail.com>
2025-11-08 16:12:45 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
